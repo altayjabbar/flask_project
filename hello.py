51b2d73e86066faf8c628e33d11ff6c9c326b667
altayjabbar <jabbarofaltay@gmail.com>
2024-07-29 19:06:41 +0400

    A Web Page With Jinja2

--- hello.py
+++ hello.py
@@ -5,12 +5,18 @@ app  =  Flask(__name__)
 # create a route derector
 @app.route('/')
 def index():
-    return render_template('index.html')
+    first_name = 'Jabbarov'
+    stuff = 'This is my firstpost'
+    favorite_pizza = ['paperroni','chesse']
+    return render_template('index.html',
+            first_name=first_name,
+            stuff = stuff,
+            favorite_pizza = favorite_pizza,)
 
 # localhost:5000/user/john
 @app.route('/user/<name>')
 def user(name):
-    return '<h1> hello {}</h1>'.format(name)
+    return render_template('user.html',user_name=name)
 
 
 
