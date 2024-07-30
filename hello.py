b736a76af3cf1571a35c8a184eb4b78a4fc9d508
altayjabbar <jabbarofaltay@gmail.com>
2024-07-30 16:15:34 +0400

    To Use Messages With Flask

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,flash
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
@@ -19,7 +19,7 @@ class NameForm(FlaskForm):
 @app.route("/")
 def index():
     first_name = "Jabbarov"
-    stuff = "This is my firstpost"
+    stuff = "This is my firstpost"  
     favorite_pizza = ["paperroni", "chesse"]
     return render_template(
         "index.html",
@@ -54,7 +54,7 @@ def name():
     if form.validate_on_submit():
         name = form.name.data
         form.name.data = ''
-
+        flash('Form Submitted Successfully')
     return render_template("name.html", name=name, form=form)
 
 
